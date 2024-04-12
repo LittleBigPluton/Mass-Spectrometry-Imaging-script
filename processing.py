11783d5c639cac8fc37b0912d3b9ff611d60a049
Umut <umtgokdemir@gmail.com>
2024-04-12 16:44:06 +0200

    Two sample data files were added. Their example cleaning process was also added. Sample output graphs were created.

--- processing.py
+++ processing.py
@@ -26,40 +26,19 @@ class data_process:
         #Initialize with file path and empty data attributes.
         self.file_path = file_path
         self.data = None
-        self.column_names = None
+        self.mz_values = None
         self.Xunique = None
         self.Yunique = None
         self.molecule = None
 
     def create_data_frame(self):
-        # Read data from a tab-separated file and set up the DataFrame.
-        # Change delimiter to use other seperations
         try:
-            # Open file again to extract column names
-            with open(self.file_path,'r') as file:
-                lines = file.readlines()
-
-            # Extract column names from the fourth line (index 3)
-            # Split by tab and strip to remove any leading/trailing whitespace
-            self.column_names = ["Index", "X", "Y"] + lines[3].strip().split('\t')
-
-            # Use lines from the fifth line onwards (index 4) for data
-            data_str = ''.join(lines[4:])
-
-            # Convert the data string into a StringIO object
-            # StringIO creates in-memory text stream from data_str
-            # to give it to the DataFrame as a virtual file
-            data_io = io.StringIO(data_str)
-
-            # Read the data into a DataFrame
-            self.data = pd.read_csv(data_io, delimiter='\t', header=None)
-
-            # Rename the columns in the DataFrame with the extracted column names
-            self.data.columns = self.column_names + list(self.data.columns[-2:])
-            # Print out first 10 rows of the data to have a sight
-            print("First the rows of the data is: ")
-            print(self.data.head(10))
-            print(f"Data includes {self.data.shape[0]} rows and {self.data.shape[1]} columns.")
+            # Read the cleaned data file from the given file path
+            self.data = pd.read_csv(self.file_path)
+            print(self.data.head())
+            print(self.data.shape)
+            # Extract mz values from the column names
+            self.mz_values = self.data.columns[2:]
 
         except FileNotFoundError as e:
             print("File not found. Please check the file path and try again.")
@@ -72,13 +51,16 @@ class data_process:
         self.Yunique = self.data['Y'].unique()
 
     def get_column_names(self):
+        # Get whole column names of the file
         print(self.data.columns)
         return self.data.columns
 
     def get_DataFrame(self):
+        # TO get data as data frame
         return self.data
 
     def set_molecule(self,molecule):
+        # To specify desired molecule to visualize
         self.molecule = str(molecule)
 
     def clean_data(self,drop_columns):
@@ -92,11 +74,10 @@ class data_process:
             print(f"Error removing columns: {e}")
 
     def normalize_by_TIC(self):
-        # Calculate Total Ion Current (TIC) for normalization,
-        # excluding 'Index', 'X', 'Y', and the last two unnecessary columns.
-        # self.column_names includes only Index, X, Y and rest of the columns
-        # except last two not necessary columns
-        self.data["TIC"] = self.data[self.column_names[3:]].sum(axis=1)
+        # Calculate Total Ion Current (TIC) for normalization
+        # Create a new column names as TIC and store whole total intensities by coordinates
+        self.data["TIC"] = self.data[self.mz_values].sum(axis=1)
         # To normalize all data, uncomment the following code but computation time is longer
-        # self.data[self.column_names[3:]]=self.data[self.column_names[3:]].div(self.data["TIC"], axis=0)
+        # self.data[self.mz_values=self.data[self.mz_values].div(self.data["TIC"], axis=0)
+        # TO normalize just one molecule by TIC
         self.data[self.molecule] = self.data[self.molecule].div(self.data["TIC"], axis = 0)
